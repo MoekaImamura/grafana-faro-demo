21998a9d91cad6f20fcbaa8ccfe60ee811dde91f
MoekaImamura <Imamura@kdl.co.jp>
2025-05-16 08:20:07 +0900

    Faro設定ファイル初期化

--- src/lib/faro.ts
+++ src/lib/faro.ts
@@ -1,23 +1,11 @@
-import {
-  getWebInstrumentations,
-  initializeFaro,
-  type Faro,
-} from '@grafana/faro-web-sdk'
-import { TracingInstrumentation } from '@grafana/faro-web-tracing'
+import { initializeFaro } from '@grafana/faro-react';
 
-export const faro: Faro = initializeFaro({
-  url: 'https://faro-collector-prod-〇〇.grafana.net/collect/xxxxxxxxxxxxxxxxxxxx', // ← Grafana Cloud で生成された Collector URL に変更
+initializeFaro({
+  // required: the URL of the Grafana collector
+  url: 'my/collector/url',
+
+  // required: the identification label of your application
   app: {
-    name: 'grafana-faro-demo',
-    version: '1.0.0',
-    environment: 'development',
+    name: 'my-react-app',
   },
-  user: {
-    id: 'user-123',
-    email: 'demo@example.com',
-  },
-  instrumentations: [
-    ...getWebInstrumentations(),
-    new TracingInstrumentation(),
-  ],
-})
+});
