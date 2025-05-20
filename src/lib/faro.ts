744004742a284551cd7afe7cf64a701ec80a0e18
MoekaImamura <Imamura@kdl.co.jp>
2025-05-20 10:12:33 +0900

    Faro設定ファイル更新

--- src/lib/faro.ts
+++ src/lib/faro.ts
@@ -1,11 +1,27 @@
-import { initializeFaro } from '@grafana/faro-react';
+import { matchRoutes } from 'react-router-dom';
+import { initializeFaro, createReactRouterV6DataOptions, ReactIntegration, getWebInstrumentations, } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 initializeFaro({
-  // required: the URL of the Grafana collector
-  url: 'my/collector/url',
-
-  // required: the identification label of your application
+  url: 'https://faro-collector-prod-ap-northeast-0.grafana.net/collect/9c11f7135b3e3a27d77f01f79228d41a',
   app: {
-    name: 'my-react-app',
+    name: 'Vite+React-app',
+    version: '1.0.0',
+    environment: 'production'
   },
+
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+
+    // React integration for React applications.
+    new ReactIntegration({
+      router: createReactRouterV6DataOptions({
+        matchRoutes,
+      }),
+    }),
+  ],
 });
